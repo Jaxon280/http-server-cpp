9dc361a40a9901e49a6c45b33dcd2daa9bcf0f4f
飯田夏生 <keresu0720@icloud.com>
2020-08-18 08:02:39 +0900

    add binding socket

--- Echo.cpp
+++ Echo.cpp
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <iostream>
 
 int main()
@@ -6,6 +7,18 @@ int main()
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
-        std::cerr << "[ERROR] Failed to create socket." return -1
+        std::cerr << "[ERROR] Failed to create socket.";
+        return -1;
+    }
+
+    sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(2850); // hard coding
+    sa.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+    {
+        std::cerr << "[ERROR] Failed to bind address to socket.";
+        return -1;
     }
 }
