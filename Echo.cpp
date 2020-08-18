d92a4ae8219bad1453380ca0af1167db2be0f6f8
飯田夏生 <keresu0720@icloud.com>
2020-08-18 09:30:48 +0900

    Add accepting connection and recv&send messages

--- Echo.cpp
+++ Echo.cpp
@@ -1,24 +1,68 @@
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include <iostream>
 
 int main()
 {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    int host_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (host_fd < 0)
     {
         std::cerr << "[ERROR] Failed to create socket.";
         return -1;
     }
 
-    sockaddr_in sa;
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(2850); // hard coding
-    sa.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in host_sa;
+    host_sa.sin_family = AF_INET;
+    host_sa.sin_port = htons(2850); // hard coding
+    host_sa.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+    if (bind(host_fd, (struct sockaddr *)&host_sa, sizeof(host_sa)) < 0)
     {
         std::cerr << "[ERROR] Failed to bind address to socket.";
         return -1;
+    }
+
+    if (listen(host_fd, SOMAXCONN) < 0)
+    {
+        std::cerr << "[ERROR] Failed to listen.";
+        return -1;
+    }
+
+    sockaddr_in remote_sa;
+    socklen_t size_remote_sa = sizeof(remote_sa);
+    int remote_fd = accept(host_fd, (sockaddr *)&remote_sa, &size_remote_sa);
+
+    char msg_buf[4096];
+    while (true)
+    {
+        if (remote_fd != -1)
+        {
+            int bytes = recv(remote_fd, &msg_buf, sizeof(msg_buf), 0);
+            if (bytes == 0)
+            {
+                std::cout << "[INFO] Client is disconnected.\n";
+                break;
+            }
+            else if (bytes < 0)
+            {
+                std::cerr << "[ERROR] Something went wrong while receiving data!.\n";
+                break;
+            }
+            else
+            {
+                std::cout << "Client sent messages:" << std::string(msg_buf, 0, bytes) << "\n";
+                if (send(remote_fd, &msg_buf, bytes, 0) < 0)
+                {
+                    std::cerr << "[ERROR] Message cannnot be send";
+                    break;
+                }
+            }
+        }
+        else
+        {
+            std::cerr << "[ERROR] Failed to accept client's connection.";
+            return -1;
+        }
     }
 }
