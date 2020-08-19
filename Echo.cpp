539a0eeaa69a5c3996b772219ed62962dac40914
飯田夏生 <keresu0720@icloud.com>
2020-08-19 14:34:13 +0900

    Fix standard-output buffering

--- Echo.cpp
+++ Echo.cpp
@@ -5,12 +5,14 @@
 
 int main()
 {
+    std::cout << "Start program" << std::endl;
     int host_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (host_fd < 0)
     {
         std::cerr << "[ERROR] Failed to create socket.";
         return -1;
     }
+    std::cout << "Socket is created." << std::endl;
 
     sockaddr_in host_sa;
     host_sa.sin_family = AF_INET;
@@ -19,50 +21,52 @@ int main()
 
     if (bind(host_fd, (struct sockaddr *)&host_sa, sizeof(host_sa)) < 0)
     {
-        std::cerr << "[ERROR] Failed to bind address to socket.";
+        std::cerr << "[ERROR] Failed to bind address to socket." << std::endl;
         return -1;
     }
+    std::cout << "Bind socket." << std::endl;
 
     if (listen(host_fd, SOMAXCONN) < 0)
     {
-        std::cerr << "[ERROR] Failed to listen.";
+        std::cerr << "[ERROR] Failed to listen." << std::endl;
         return -1;
     }
+    std::cout << "Start listening connection..." << std::endl;
 
     sockaddr_in remote_sa;
     socklen_t size_remote_sa = sizeof(remote_sa);
     int remote_fd = accept(host_fd, (sockaddr *)&remote_sa, &size_remote_sa);
+    if (remote_fd < 0)
+    {
+        std::cerr << "[ERROR] Failed to accept client's connection." << std::endl;
+        return -1;
+    }
+    std::cout << "Accept client's connection" << std::endl;
 
     char msg_buf[4096];
     while (true)
     {
-        if (remote_fd != -1)
+        int bytes = recv(remote_fd, &msg_buf, sizeof(msg_buf), 0);
+        if (bytes == 0)
         {
-            int bytes = recv(remote_fd, &msg_buf, sizeof(msg_buf), 0);
-            if (bytes == 0)
-            {
-                std::cout << "[INFO] Client is disconnected.\n";
-                break;
-            }
-            else if (bytes < 0)
-            {
-                std::cerr << "[ERROR] Something went wrong while receiving data!.\n";
-                break;
-            }
-            else
-            {
-                std::cout << "Client sent messages:" << std::string(msg_buf, 0, bytes) << "\n";
-                if (send(remote_fd, &msg_buf, bytes, 0) < 0)
-                {
-                    std::cerr << "[ERROR] Message cannnot be send";
-                    break;
-                }
-            }
+            std::cout << "[INFO] Client is disconnected.\n"
+                      << std::endl;
+            break;
+        }
+        else if (bytes < 0)
+        {
+            std::cerr << "[ERROR] Something went wrong while receiving data!.\n"
+                      << std::endl;
+            break;
         }
         else
         {
-            std::cerr << "[ERROR] Failed to accept client's connection.";
-            return -1;
+            std::cout << "Client sent messages:" << std::string(msg_buf, 0, bytes) << std::endl;
+            if (send(remote_fd, &msg_buf, bytes, 0) < 0)
+            {
+                std::cerr << "[ERROR] Message cannnot be send" << std::endl;
+                break;
+            }
         }
     }
 }
